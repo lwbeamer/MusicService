44ab628b0fce1a6afba1be0251a08d6a5f0fc2e9
Daniil <313089@niuitmo.ru>
2022-10-05 02:07:41 +0300

    Added options in properties, added Role and User entity for work with DB

---
+++ server/src/main/java/com/example/server/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.server.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
