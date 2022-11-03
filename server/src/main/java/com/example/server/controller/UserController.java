e6d10cdfb9b7ca7948249f4af80aea90dc88f12c
SLNSL <nik231002@mail.ru>
2022-11-03 17:23:53 +0300

    get album by id fix

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getLastAlbums(getLastAlbums.getCount()));
     }
 
-    @PostMapping("/getAlbumById/getAlbumById")
+    @PostMapping("/getAlbumById")
     public ResponseEntity<?> getAlbumById(@RequestBody GetAlbumRequest getAlbumRequest) {
         return ResponseEntity.ok().body(userService.getAlbumById(getAlbumRequest.getAlbumId()));
     }
