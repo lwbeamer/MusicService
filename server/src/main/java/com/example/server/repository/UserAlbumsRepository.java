55856fdcb8955ba7cd6196fc114368b9d9ae201d
susaasus1 <313089@niuitmo.ru>
2022-11-06 01:03:07 +0300

    Update

--- server/src/main/java/com/example/server/repository/UserAlbumsRepository.java
+++ server/src/main/java/com/example/server/repository/UserAlbumsRepository.java
@@ -4,6 +4,7 @@ import com.example.server.entity.Album;
 import com.example.server.entity.UserAlbums;
 import com.example.server.entity.Uzer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,6 +18,7 @@ public interface UserAlbumsRepository extends JpaRepository<UserAlbums, Long> {
 
     @Query(value = "SELECT * FROM (Select * FROM user_albums ORDER BY id DESC LIMIT :count) albums ", nativeQuery = true)
     Optional<List<UserAlbums>> getLastAlbums(@Param("count") Long count);
+    @Modifying
     @Query(value = "DELETE FROM uzer_albums_songs WHERE id_user_albums = :idUserAlbums and id_song = :songId",nativeQuery = true)
     void deleteSongFromUserPlaylist(@Param("idUserAlbums") Long idUserAlbums,@Param("songId") Long songId);
 }
